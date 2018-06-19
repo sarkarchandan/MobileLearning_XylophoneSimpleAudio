653390677a522a528de5070a72ae16f354c28cca
chandan <elitegreen@gmail.com>
2018-06-20 01:12:08 +0200

    Initial Commit

---
+++ XylophoneParentAppBase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XylophoneParentAppBase
+//
+//  Created by Chandan Sarkar on 20.06.18.
+//  Copyright © 2018 Chandan Sarkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XylophoneParentAppBase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XylophoneParentAppBase
+//
+//  Created by Chandan Sarkar on 20.06.18.
+//  Copyright © 2018 Chandan Sarkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
